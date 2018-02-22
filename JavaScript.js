cc020612eb0c30ec5af5229e183daf0e8f57afd8
shimizu-seita <tanoshikuikoyo@gmail.com>
2018-02-23 02:08:49 +0900

    JavaScriptの2進整数の乗算を行う手順を実装したプログラム

---
+++ JavaScript.js
@@ -0,0 +1,17 @@
+﻿var x=10, y=20, z=0, i=1;
+while(true) {
+    document.write('i='+i+'( ');
+    //yの0ビット目が1かを確認
+    if ((y & 1) > 0) {
+        z = z + x;
+    }
+    document.write('y[0]='+(y & 1)+', z='+z);  
+    x = x << 1; //xを1ビット左シフト
+    y = y >> 1; //yを1ビット右シフト
+    i = i + 1;
+    document.write(', x='+x+', y='+y+' )<br>');
+    if (i > 16) {
+        break;
+    }
+}
+document.write('<b>出力Z='+z+'</b>');
